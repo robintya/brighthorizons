dd26e63ed52a8df64925be623deaa3a616d80c37
Robin Tyagi <robintyagi@192.168.1.3>
2025-01-17 15:30:12 +0530

    Added Timeouts.java

---
+++ src/test/java/com/edu/config/Timeouts.java
@@ -0,0 +1,11 @@
+package com.edu.config;
+
+public class Timeouts {
+    public static final int SHORT = 5;
+    public static final int DEFAULT = 10;
+    public static final int MEDIUM = 15;
+    public static final int LONG = 30;
+    public static final int ZERO = 0;
+    public static final int DELAY = 500;
+    public static final int CUSTOM = 3000;
+}
